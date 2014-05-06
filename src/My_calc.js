d575f0e644023413e2241db6adf61286cc1eec5d
Maryna <marchuk.maryna@gmail.com>
2014-05-06 22:59:34 +0300

    some improvenments...

--- src/My_calc.js
+++ src/My_calc.js
@@ -1,3 +1,23 @@
 /**
  * Created by Svetttik on 01.05.14.
  */
+
+
+window.addEventListener('load',function(){
+
+    var btn = document.getElementById('someId');
+    btn.addEventListener('click', function(){
+        var value = this.value;
+        var input = document.getElementById('input');
+        input.value = input.value + value;
+
+    });
+
+});
+
+
+
+
+
+
+
