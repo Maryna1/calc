acfef563d7ab42f9cd719d1b81643c8cf6ea2439
maryna <marino4kam@list.ru>
2014-05-10 13:19:13 +0300

    C

--- src/My_calc.js
+++ src/My_calc.js
@@ -5,15 +5,117 @@
 
 window.addEventListener('load',function(){
 
-    var btn = document.getElementById('someId');
-    btn.addEventListener('click', function(){
+    var btnone = document.getElementById('btn1');
+    btnone.addEventListener('click', function(){
         var value = this.value;
         var input = document.getElementById('input');
-        input.value = input.value + value;
-
+        input.value = value;
     });
 
+    var btntwo = document.getElementById('btn2');
+    btntwo.addEventListener('click', function(){
+        var value = this.value;
+        var input = document.getElementById('input');
+        input.value = value;
+    });
+
+    var btnthree = document.getElementById('btn3');
+    btnthree.addEventListener('click', function(){
+        var value = this.value;
+        var input = document.getElementById('input');
+        input.value = value;
+    });
+
+    var btnfour = document.getElementById('btn4');
+    btnfour.addEventListener('click', function(){
+        var value = this.value;
+        var input = document.getElementById('input');
+        input.value = value;
+    });
+
+    var btnfive = document.getElementById('btn5');
+    btnfive.addEventListener('click', function(){
+        var value = this.value;
+        var input = document.getElementById('input');
+        input.value = value;
+    });
+
+    var btnsix = document.getElementById('btn6');
+    btnsix.addEventListener('click', function(){
+        var value = this.value;
+        var input = document.getElementById('input');
+        input.value = value;
+    });
+
+    var btnseven = document.getElementById('btn7');
+    btnseven.addEventListener('click', function(){
+        var value = this.value;
+        var input = document.getElementById('input');
+        input.value = value;
+    });
+
+    var btneight = document.getElementById('btn8');
+    btneight.addEventListener('click', function(){
+        var value = this.value;
+        var input = document.getElementById('input');
+        input.value = value;
+    });
+
+    var btnnine = document.getElementById('btn9');
+    btnnine.addEventListener('click', function(){
+        var value = this.value;
+        var input = document.getElementById('input');
+        input.value = value;
+    });
+
+    var btnnull = document.getElementById('btn0');
+    btnnull.addEventListener('click', function(){
+        var value = this.value;
+        var input = document.getElementById('input');
+        input.value = value;});
+
+    var btnp = document.getElementById('btnpoint');
+    btnp.addEventListener('click', function(){
+        var value = this.value;
+        var input = document.getElementById('input');
+        input.value = input.value +value;});
+
+//    var btn = document.getElementById('btn+');
+//    btn.addEventListener('click', function(){
+//        var value = this.value;
+//        var input = document.getElementById('input');
+//        input.value = value;});
+//
+//    var btn = document.getElementById('btn-');
+//    btn.addEventListener('click', function(){
+//        var value = this.value;
+//        var input = document.getElementById('input');
+//        input.value = value;});
+//
+//    var btn = document.getElementById('btn/');
+//    btn.addEventListener('click', function(){
+//        var value = this.value;
+//        var input = document.getElementById('input');
+//        input.value = value;});
+//
+//    var btn = document.getElementById('btnx');
+//    btn.addEventListener('click', function(){
+//        var value = this.value;
+//        var input = document.getElementById('input');
+//        input.value = value;});
+
+    var btnC = document.getElementById('btnC');
+    btnC.addEventListener('click', function(){
+        var input = document.getElementById('input');
+        input.value = 0;
+    });
+
+    var btnequel = document.getElementById('btn=');
+    btnequel.addEventListener('click', function(){
+    });
 });
+
+
 
 
 
