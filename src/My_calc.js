4000959367ead29c10c5931cbc54efe8f9930a9d
Maryna <marchuk.maryna@gmail.com>
2014-05-13 22:45:01 +0300

    minus and dot

--- src/My_calc.js
+++ src/My_calc.js
@@ -8,169 +8,112 @@ window.addEventListener('load',function (){
     var btnC = document.getElementById('btnC');
     btnC.addEventListener('click', function(){
         var input = document.getElementById('input');
-        input.value = 0;
-    });
+        input.value = 0;});
 
     var btnone = document.getElementById('btn1');
     btnone.addEventListener('click', function(){
-        var value = this.value;
-        var input = document.getElementById('input');
-        input.value = input.value+ this.value;
+        print(this.value);
     });
+
 
     var btntwo = document.getElementById('btn2');
     btntwo.addEventListener('click', function(){
-        var value = this.value;
-        var input = document.getElementById('input');
-        input.value =input.value+ value;
+        print(this.value);
     });
 
     var btnthree = document.getElementById('btn3');
     btnthree.addEventListener('click', function(){
-        var value = this.value;
-        var input = document.getElementById('input');
-        input.value =input.value+ value;
+        print(this.value);
     });
 
     var btnfour = document.getElementById('btn4');
     btnfour.addEventListener('click', function(){
-        var value = this.value;
-        var input = document.getElementById('input');
-        input.value =input.value+ value;
+        print(this.value);
     });
 
     var btnfive = document.getElementById('btn5');
     btnfive.addEventListener('click', function(){
-        var value = this.value;
-        var input = document.getElementById('input');
-        input.value = input.value+value;
+        print(this.value);
     });
 
     var btnsix = document.getElementById('btn6');
     btnsix.addEventListener('click', function(){
-        var value = this.value;
-        var input = document.getElementById('input');
-        input.value =input.value+ value;
+        print(this.value);
     });
 
     var btnseven = document.getElementById('btn7');
     btnseven.addEventListener('click', function(){
-        var value = this.value;
-        var input = document.getElementById('input');
-        input.value =input.value+ value;
+        print(this.value);
     });
 
     var btneight = document.getElementById('btn8');
     btneight.addEventListener('click', function(){
-        var value = this.value;
-        var input = document.getElementById('input');
-        input.value =input.value+value;
+        print(this.value);
     });
 
     var btnnine = document.getElementById('btn9');
     btnnine.addEventListener('click', function(){
-        var value = this.value;
-        var input = document.getElementById('input');
-        input.value =input.value+ value;
+        print(this.value);
     });
 
     var btnnull = document.getElementById('btn0');
     btnnull.addEventListener('click', function(){
-        var value = this.value;
-        var input = document.getElementById('input');
-        input.value =input.value+ value;});
+        print(this.value);
+    });
 
     var btnp = document.getElementById('btnpoint');
     btnp.addEventListener('click', function(){
-        var value = this.value;
-        var input = document.getElementById('input');
-        input.value = input.value +value;});
-
-    var btns=document.getElement;
-    btns.addEventListener('click', function(){
-    var a=this.value;
-    alert(a);
+        print(this.value);
     });
 
-    var btnsumm = document.getElementById('btn+');
-    btnsumm.addEventListener('click', function(){
-        var value = this.value;
-        var input = document.getElementById('input');
-        input.value = value;});
-//
-//    var btn = document.getElementById('btn-');
-//    btn.addEventListener('click', function(){
-//        var value = this.value;
-//        var input = document.getElementById('input');
-//        input.value = value;});
-//
-//    var btn = document.getElementById('btn/');
-//    btn.addEventListener('click', function(){
-//        var value = this.value;
-//        var input = document.getElementById('input');
-//        input.value = value;});
-//
-//    var btn = document.getElementById('btnx');
-//    btn.addEventListener('click', function(){
-//        var value = this.value;
-//        var input = document.getElementById('input');
-//        input.value = value;});
-    var btnequel = document.getElementById('btn=');
-    btnequel.addEventListener('click', function(){
-return 0;});
+    var btnminus = document.getElementById('btn+-');
+    btnminus.addEventListener('click', function(){
+        print(this.value);
+    });
 });
 
-function culc(){
-    var a;
-    var b;
-    var btn1 = document.getElementById('btn1');
-    var btn2 = document.getElementById('btn2');
-    var btn3 = document.getElementById('btn3');
-    var btn4 = document.getElementById('btn4');
-    var btn5 = document.getElementById('btn5');
-    var btn6 = document.getElementById('btn6');
-    var btn7 = document.getElementById('btn7');
-    var btn8 = document.getElementById('btn8');
-    var btn9 = document.getElementById('btn9');
-    var btn0 = document.getElementById('btn0');
 
-    if (btn1.addEventListener('click')){
-        a=btn1.value;
+function print(symbol){
+
+    if (!check(symbol)){
+        return;
     }
-    else if (btn2.addEventListener('click')){
-        a=btn2.value;
+    var input = document.getElementById('input');
+
+
+    console.log(symbol);
+    if ( symbol=='.' ){
+        if (input.value.indexOf('.')==-1){
+             input.value =input.value + symbol;
+        }
+        else {
+            return;
+        }
     }
-    else if (btn3.addEventListener('click')){
-        a=btn3.value;
+    else if ( symbol=='-' ){
+        if (input.value.indexOf('-')==-1){
+            input.value = symbol + input.value ;
+        }
+        else {
+            input.value=input.value.replace('-','');
+        }
     }
-    else if (btn4.addEventListener('click')){
-        a=btn4.value;
+    else{
+        if (input.value==0){
+            input.value = symbol;
+        }else{
+            input.value = input.value + symbol;
+        }
     }
-    else if (btn5.addEventListener('click')){
-        a=btn5.value;
-    }
-    else if (btn6.addEventListener('click')){
-        a=btn6.value;
-    }
-    else if (btn7.addEventListener('click')){
-        a=btn7.value;
-    }
-    else if (btn8.addEventListener('click')){
-        a=btn8.value;
-    }
-    else if (btn9.addEventListener('click')){
-        a=btn9.value;
-    }
-    else if (btn0.addEventListener('click')){
-        a=btn0.value;
-    }
-    return alert(a)
+    return;
 }
 
-
-
-
-
-
-
+function check(symbol){
+    if (symbol==1 || symbol==2||symbol==3||symbol==4||symbol==5||symbol==6||symbol==7||symbol==8||symbol==9||symbol==0||symbol=='.'||symbol=='-'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
